f096e3796612eac9112f2a580f8b13ea3cf259d1
gasp8918 <gasp8918@student.acitech.org>
2024-09-23 09:42:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,20 +4,19 @@ import random as r
 def gen_list():
     listy = []
     for x in range(10):
-        listy.append(r.randint(-100,100))
+        listy.append(r.randint(-100, 100))
     return listy
 
 
-def bubble(sorter): # sorter is the list we are sorting
-    unsorted = True
-    while unsorted:
-        yummers = False
-        for x in range(len(sorter)):
+def bubble(sorter):  # sorter is the list we are sorting
+    while True:
+        yummers = True
+        for x in range(len(sorter) - 1):
             print(sorter)
             first = sorter[x]
-            next = sorter[(x+1)]
-            if (first > next):
-                sorter[x+1] = first
+            next = sorter[(x + 1)]
+            if first > next:
+                yummers = False
+                sorter[x + 1] = first
                 sorter[x] = next
-        if yummers: unsorted = False
-bubble(gen_list())
+        if yummers: break
