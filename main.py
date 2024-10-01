24fc6b8ce235d93163c5d9d2da33b31c0749b90f
gasp8918 <127219057+gasp8918@users.noreply.github.com>
2024-10-01 09:41:43 -0400

    COMMENT

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ def gen_list():
 def bubble(sorter):  # sorter is the list we are sorting
     while True:
         yummers = True
-        for x in range(len(sorter) - 1):
+        for x in range(len(sorter) - 1):  # for the length of the list
             print(sorter)
-            first = sorter[x]
-            next = sorter[(x + 1)]
-            if first > next:
-                yummers = False
-                sorter[x + 1] = first
+            first = sorter[x]  # make a variable for the first number we are looking at
+            next = sorter[(x + 1)]  # and second
+            if first > next:  # and if the first number is large than the second,
+                yummers = False  # make sure to flag we've changed something this loop,
+                sorter[x + 1] = first  # then we flip the numbers in the list
                 sorter[x] = next
-        if yummers: break
+        if yummers: break  # if this variable has not been changed in the loop, that means the sort is done.
